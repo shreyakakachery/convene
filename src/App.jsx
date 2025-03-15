f520ab14a25555c5890972ae4fba95e7b94ca9c7
Shreya Kakachery <shreyakakachery@gmail.com>
2025-03-14 19:14:40 -0700

    Connect backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,39 @@
-import { useState } from 'react'
+import { BACKEND_URL } from "./scripts/config.js";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import './App.scss'
 
 function App() {
 
+  console.log("hi")
+
+  const [places, setPlaces] = useState(null);
+
+  console.log(places)
+
+
+  const fetchPlaces = async () => {
+    try {
+      // const response = await axios.get(`${BACKEND_URL}/places?lat=49.2827&lon=-123.1207`);
+      const response = await axios.get(`${BACKEND_URL}/places`);
+      setPlaces(response.data);
+    } catch (error) {
+      console.error("Error fetching places:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchPlaces();
+  }, []);
+
+  console.log(places)
+
+
+
   return (
     <div>
       <p>App.jsx</p>
+      <p>{BACKEND_URL}</p>
     </div>
 
   )
