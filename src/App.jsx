660391121b56c703eede239b1320e386f3ae654b
Shreya Kakachery <shreyakakachery@gmail.com>
2025-03-17 09:19:10 -0700

    Create NotFound page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import axios from "axios";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header.jsx";
 import HomePage from "./pages/HomePage/HomePage.jsx";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage.jsx";
 import Footer from "./components/Footer/Footer.jsx";
 import "./App.scss";
 
@@ -127,6 +128,7 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Routes>
         <Footer />
       </BrowserRouter>
---
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -0,0 +1,11 @@
+
+function NotFoundPage() {
+  return (
+    <div>
+        <p>404 Error: Not Found</p>
+        <p>Please use the link to navigate back to the main page</p>
+    </div>
+  )
+}
+
+export default NotFoundPage
