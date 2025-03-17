0ab16d9cd4d3c382137bdf2c0a7feeb03357126b
Shreya Kakachery <shreyakakachery@gmail.com>
2025-03-17 10:48:38 -0700

    Get stop pairs in HomePage and pass to StopPairsList

--- src/components/StopPairsList/StopPairsList.jsx
+++ src/components/StopPairsList/StopPairsList.jsx
@@ -1,11 +1,54 @@
-import React from 'react'
 
-function StopPairsList() {
+
+function StopPairsList({stopPairs}) {
   return (
     <div>
-        <p>StopPairsList.jsx</p>
+      <p>StopPairsList.jsx</p>
+      <h1>STOP PAIRS</h1>
+      {stopPairs.length > 0 ? (
+        stopPairs.map((stopPair, index) => (
+          <div key={index}>
+            <h2>Stop Pair {index + 1}</h2>
+            <p>
+              <strong>Distance:</strong> {stopPair.distance} km
+            </p>
+
+            <div>
+              <h3>Route A Stop:</h3>
+              <p>
+                <strong>Route Name:</strong> {stopPair.routeA_stop.route_name}
+              </p>
+              <p>
+                <strong>Stop Name:</strong> {stopPair.routeA_stop.stop_name}
+              </p>
+              <p>
+                <strong>Stop Sequence:</strong>{" "}
+                {stopPair.routeA_stop.stop_sequence}
+              </p>
+            </div>
+
+            <div>
+              <h3>Route B Stop:</h3>
+              <p>
+                <strong>Route Name:</strong> {stopPair.routeB_stop.route_name}
+              </p>
+              <p>
+                <strong>Stop Name:</strong> {stopPair.routeB_stop.stop_name}
+              </p>
+              <p>
+                <strong>Stop Sequence:</strong>{" "}
+                {stopPair.routeB_stop.stop_sequence}
+              </p>
+            </div>
+
+            <hr />
+          </div>
+        ))
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
-  )
+  );
 }
 
-export default StopPairsList
+export default StopPairsList;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -13,25 +13,25 @@ function HomePage() {
 
   //   get nearby routes
 
-  const fetchRoutes = async () => {
-    try {
-      const response = await axios.get(
-        `${BACKEND_URL}/routes?locA=2329%20West%20Mall&locB=3551%20Foster%20Avenue`
-      );
+//   const fetchRoutes = async () => {
+//     try {
+//       const response = await axios.get(
+//         `${BACKEND_URL}/routes?locA=2329%20West%20Mall&locB=3551%20Foster%20Avenue`
+//       );
 
-      setRoutes(response.data);
-    } catch (error) {
-      console.error("Error fetching places:", error);
-    }
-  };
+//       setRoutes(response.data);
+//     } catch (error) {
+//       console.error("Error fetching places:", error);
+//     }
+//   };
 
-  useEffect(() => {
-    fetchRoutes();
-  }, []);
+//   useEffect(() => {
+//     fetchRoutes();
+//   }, []);
 
-  if (!routes) {
-    return <div>Loading nearby routes...</div>;
-  }
+//   if (!routes) {
+//     return <div>Loading nearby routes...</div>;
+//   }
 
   //   get stop pairs
 
@@ -67,13 +67,13 @@ function HomePage() {
     }
   };
 
-  //   useEffect(() => {
-  //     fetchStopPairs();
-  //   }, []);
+    useEffect(() => {
+      fetchStopPairs();
+    }, []);
 
-  //   if (!stopPairs) {
-  //     return <div>Loading stop pairs...</div>;
-  //   }
+    if (!stopPairs) {
+      return <div>Loading stop pairs...</div>;
+    }
 
   //   get places
 
@@ -98,55 +98,12 @@ function HomePage() {
   return (
     <div>
       <p>HomePage.jsx</p>
-      <RoutesList routes={routes} />
-      <StopPairsList />
-      <PlacesList places={places} />
+      {/* <RoutesList routes={routes} /> */}
+      <StopPairsList stopPairs={stopPairs} />
+      {/* <PlacesList places={places} /> */}
 
-      {/* <h1>NEARBY ROUTES</h1>
-      {routes.length > 0 ? (
-        routes.map((route, routeIndex) => (
-          <div key={routeIndex}>
-            <h2>Address: {route.address}</h2>
-
-            <h3>Stops:</h3>
-            {route.filteredStops.map((stop, stopIndex) => (
-              <div
-                key={stopIndex}
-                style={{
-                  marginBottom: "10px",
-                  padding: "10px",
-                  border: "1px solid gray",
-                }}
-              >
-                <p>
-                  <strong>Route:</strong> {stop.route || "N/A"}
-                </p>
-                <p>
-                  <strong>Stop Name:</strong> {stop.stop_name}
-                </p>
-                <p>
-                  <strong>Stop Code:</strong> {stop.stop_code}
-                </p>
-                <p>
-                  <strong>Stop ID:</strong> {stop.stop_id}
-                </p>
-                <p>
-                  <strong>Distance:</strong>{" "}
-                  {stop.distance ? `${stop.distance.toFixed(2)} km` : "N/A"}
-                </p>
-                <p>
-                  <strong>Coordinates:</strong> {stop.stop_lat}, {stop.stop_lon}
-                </p>
-              </div>
-            ))}
-            <hr />
-          </div>
-        ))
-      ) : (
-        <p>Loading...</p>
-      )} */}
-
-      {/* <h1>STOP PAIRS</h1>
+{/* 
+      <h1>STOP PAIRS</h1>
       {stopPairs.length > 0 ? (
         stopPairs.map((stopPair, index) => (
           <div key={index}>
