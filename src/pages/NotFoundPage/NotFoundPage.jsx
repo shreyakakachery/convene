2e4fcd173a199e1ab4f5089b4a1c4f94b40fd17e
Shreya Kakachery <shreyakakachery@gmail.com>
2025-03-23 21:10:42 -0700

    Add back button on Not Found Page

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,7 +1,18 @@
+import { useNavigate } from "react-router-dom";
+
 function NotFoundPage() {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <p>404 Error: Not Found</p>
+    <div className="not-found-page">
+      <p className="not-found-page__message">404 Error: Page Not Found</p>
+      <button
+        className="not-found-page__back-btn"
+        onClick={() => navigate("/")}
+      >
+        {" "}
+        Take me to Home Page
+      </button>
     </div>
   );
 }
