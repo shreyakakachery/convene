86c707a2a57af73a3a12e3c580a6d493963796fc
Shreya Kakachery <shreyakakachery@gmail.com>
2025-04-05 15:30:20 -0700

    Add donation link, but commented out

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,8 +3,29 @@ import "./Footer.scss";
 function Footer() {
   return (
     <div className="footer">
-      <p className="footer__flair">🚌 ࿐ ࿔*:･ﾟ</p>
-      <p className="footer__copyright">&copy; Shreya Kakachery 2025</p>
+      <div className="footer__left">
+        <h4 className="footer__flair">🚌 ࿐ ࿔*:･ﾟ</h4>
+        <h4 className="footer__copyright">&copy; Shreya Kakachery 2025</h4>
+      </div>
+      <div className="footer__right">
+        {/* <a
+          href="https://www.buymeacoffee.com/yourusername"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="footer__donate-link"
+        >
+          🍦 Buy me an ice cream!
+        </a> */}
+        {/* <a
+          href="https://www.buymeacoffee.com/yourusername"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="footer__donate-link footer__donate-link--button"
+        >
+          <span className="footer__donate-link-icon">🍦</span>
+          <span className="footer__donate-link-text">Buy me an ice cream!</span>
+        </a> */}
+      </div>
     </div>
   );
 }
