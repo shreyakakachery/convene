07d33cc24e6f09f826bc03a862b33920c7f9706b
Shreya Kakachery <shreyakakachery@gmail.com>
2025-04-05 23:23:12 -0700

    Add and style instructions on home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -16,19 +16,55 @@ function HomePage() {
         <BaseMap />
       </div>
       <div className="home-page__info-container">
-        <h2>Instructions</h2>
-        <ol>
-          <li>Enter Address</li>
-          <li>Select a route from each list</li>
-          <li>Select an intersection</li>
-          <li>Browse places!</li>
-        </ol>
+        <h1 className="home-page__slogan">Meeting in the middle, made easy.</h1>
+
+        <h2 className="home-page__instructions-title">HOW TO USE:</h2>
+        <div className="home-page__instructions-box">
+          <div>
+            <h3 className="home-page__step">1 Enter Starting Locations</h3>
+            <h4 className="home-page__step-info">
+              🔎 Your address & your friends address.
+            </h4>
+          </div>
+          <div>
+            <h3 className="home-page__step">2 Select Routes</h3>
+            <h4 className="home-page__step-info">
+              🚌 Pick a nearby route from each location.
+            </h4>
+            {/* <h4 className="home-page__step-info">
+              🗺 visualize nearby routes on the map
+            </h4> */}
+          </div>
+          <div>
+            <h3 className="home-page__step">3 Select Intersection</h3>
+            <h4 className="home-page__step-info">
+              🚏 This shows where each of you will get off.
+            </h4>
+          </div>
+          <div>
+            <h3 className="home-page__step">4 Browse Places</h3>
+            <h4 className="home-page__step-info">
+              ☕ Explore cafes & restaurants around your intersection.
+            </h4>
+          </div>
+          <div>
+            <h3 className="home-page__step">5 Give Feedback</h3>
+            <h4 className="home-page__step-info">
+              💖 What did you like about this website?
+            </h4>
+            <h4 className="home-page__step-info">⚒ What can be improved?</h4>
+            <h4 className="home-page__step-info">
+              💭 Let me know what you think!
+            </h4>
+          </div>
+        </div>
+
         <button
           className="home-page__start-btn"
           onClick={() => navigate("/address")}
         >
           {" "}
-          Start
+          Let's Go!
         </button>
       </div>
     </div>
