be6f19ee9c876d7669632d2830e266c6444f04d8
Shreya Kakachery <shreyakakachery@gmail.com>
2025-04-05 22:23:31 -0700

    Style feedback form

--- src/components/FeedbackForm/FeedbackForm.jsx
+++ src/components/FeedbackForm/FeedbackForm.jsx
@@ -1,8 +1,8 @@
 // Make sure to run npm install @formspree/react
 // For more help visit https://formspr.ee/react-help
-import React from 'react';
-import { useForm, ValidationError } from '@formspree/react';
-import "./FeedbackForm.scss"
+import React from "react";
+import { useForm, ValidationError } from "@formspree/react";
+import "./FeedbackForm.scss";
 
 function FeedbackForm() {
   const [state, handleSubmit] = useForm("mjkyannj"); // your form ID from Formspree
@@ -12,47 +12,23 @@ function FeedbackForm() {
   }
 
   return (
-    <form  action="https://formspree.io/f/mjkyannj" method="POST" onSubmit={handleSubmit}>
-      <label htmlFor="name">
-        Name
-      </label>
-      <input
-        id="name"
-        type="text"
-        name="name"
-        required
-      />
-      
-      <label htmlFor="email">
-        Email Address
-      </label>
-      <input
-        id="email"
-        type="email"
-        name="email"
-        required
-      />
-      <ValidationError 
-        prefix="Email" 
-        field="email"
-        errors={state.errors}
-      />
+    <form
+      action="https://formspree.io/f/mjkyannj"
+      method="POST"
+      onSubmit={handleSubmit}
+    >
+      <label htmlFor="name">Name</label>
+      <input id="name" type="text" name="name" required />
 
-      <label htmlFor="message">
-        Feedback
-      </label>
-      <textarea
-        id="message"
-        name="message"
-        required
-      />
-      <ValidationError 
-        prefix="Message" 
-        field="message"
-        errors={state.errors}
-      />
+      <label htmlFor="email">Email Address</label>
+      <input id="email" type="email" name="email" required />
+      <ValidationError prefix="Email" field="email" errors={state.errors} />
 
-      <button type="submit" disabled={state.submitting}>
+      <label htmlFor="message">Feedback</label>
+      <textarea id="message" name="message" required />
+      <ValidationError prefix="Message" field="message" errors={state.errors} />
+
+      <button className="submit-btn" type="submit" disabled={state.submitting}>
         Submit
       </button>
     </form>
